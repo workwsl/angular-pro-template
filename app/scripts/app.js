ae9a5ea1a37fac10ded63189ad58d45b5eb25fb9
flow <workwsl@163.com>
2016-01-05 17:30:53 +0800

    angular-Boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myAngularApp
+ * @description
+ * # myAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myAngularApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
